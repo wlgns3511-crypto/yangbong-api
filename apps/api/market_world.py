b8e7130de6d8a93839a7193a03955458e8c7b3fe
LEEJIHOON <wlgns3511@gmail.com>
2025-11-07 20:31:26 +0900

    Update market_world.py

--- apps/api/market_world.py
+++ apps/api/market_world.py
@@ -6,7 +6,8 @@ from typing import Any, Dict, List
 
 import logging, requests
 
-from .market_common import get_cache, set_cache, normalize_item
+from .market_common import normalize_item
+from .cache import get_cache, set_cache
 
 
 
